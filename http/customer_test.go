16e506846628be4a5b183f9e118a0f58c359e8db
Tom Lorentsen <tom@thomaslorentsen.co.uk>
2018-07-02 22:50:46 +0100

    Test charging a customer

--- http/customer_test.go
+++ http/customer_test.go
@@ -186,6 +186,40 @@ func TestUpdateCustomer(t *testing.T) {
 	}
 }
 
+func TestChargeCustomer(t *testing.T) {
+	body := strings.NewReader("{\"customer\": \"cus_C3MQXNRknd5e6p\", \"amount\": 720, \"desc\": \"example\", \"trans_id\": \"tnx_1234\", \"business_name\": \"RoundPartner\"}")
+	stripe.Key = util.GetTestKey()
+	rr := httptest.NewRecorder()
+	req, _ := http.NewRequest("POST", "/charge", body)
+	rs := New()
+	rs.router().ServeHTTP(rr, req)
+
+	if rr.Code != http.StatusOK {
+		t.Fail()
+	}
+
+	if "application/json; charset=utf-8" != rr.Header().Get("Content-Type") {
+		t.Fail()
+	}
+
+	charge := &stripe.Charge{}
+
+	decoder := json.NewDecoder(rr.Body)
+	decoder.Decode(charge)
+
+	if 720 != charge.Amount {
+		t.Fail()
+	}
+
+	if "succeeded" != charge.Status {
+		t.Fail()
+	}
+
+	if false == charge.Paid {
+		t.Fail()
+	}
+}
+
 func TestReloadCustomers(t *testing.T) {
 	stripe.Key = util.GetTestKey()
 	rr := httptest.NewRecorder()
